c3b0179f4bf31a7f2454fc124b1320115199815f
zm-intern06 <79933310+zm-intern06@users.noreply.github.com>
2021-04-21 18:13:58 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "storage" {
-  name                     = "terraformtest"
+  name                     = "testterraf"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = "westeurope"
   account_tier             = "Standard"
